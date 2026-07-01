7be49ac573c0d7caf1d7dfd728f8dfb753e72979
white-king17 <learningdhanush@gmail.com>
2026-07-01 10:46:50 +0530

    Changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,10 +18,23 @@ export default function LoginPage() {
 
         <form
           className="mt-6 flex flex-col gap-4"
-          onSubmit={(e) => {
+          onSubmit={async (e) => {
             e.preventDefault();
-            console.log(email);
-            console.log(password);
+
+            const response = await fetch("http://localhost:5000/login", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                email,
+                password,
+              }),
+            });
+
+            const data = await response.json();
+
+            console.log(data);
           }}
         >
           
